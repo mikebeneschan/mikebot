9ca6f5f7f8a48ebcd537efc09cf1eaf6e68f7ff5
mikebeneschan <52139261+mikebeneschan@users.noreply.github.com>
2021-05-05 17:55:27 -0700

    Update mikebot.py

--- mikebot.py
+++ mikebot.py
@@ -11,6 +11,8 @@ import random
 client = discord.Client()
 guild = discord.Guild
 
+token = os.getenv('DISCORD_TOKEN')
+
 mikeQuotes = ["_decisions have consequences_","_i've killed more men with words_","poop","beep boop","oh hi mark","you've been blinded, fool","why the fuck are they nerfing bard","oh hell no","https://www.youtube.com/watch?v=uH0hikcwjIA",":0 yes that's me","math is pretty cool","teemo is the best champion in the game","that was not very cash money of you","that's a money move right there",":eyes: what's up gamers","jugilae????", "we must dance to shame dog planet","dude i feel that","dude that reminds me of when i was in popreka, ucsc's finest kpop dance group","cki is pain", "when will jeff carry me", "your honor, that's cap", "im a macro player","schnucki", "please look at this german content \nhttps://www.youtube.com/watch?v=39UDZMgPg5k"]
 
 mike8ball = ["oh heck yeah","oh hell no","yis","no lmao",":0 sure",":eyes: maybe","yes swear on me mum","nO","oui oui","NOPE","i mean i guess so","no that sounds spooky","??? i don't know","yes as long as there's no xerath involved","unfortunately no, gamer", "i mean okay yeah","no your honor","im gonna go with a yes your honor",":regional_indicator_n::regional_indicator_o:","yes but only with teemo","lmao","yikes","yes i believe","no :'0"]
