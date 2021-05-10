2c4ca6139f30783015bd652fc82ae5fb59de2968
mikebeneschan <52139261+mikebeneschan@users.noreply.github.com>
2021-05-10 15:20:32 -0700

    Add files via upload

---
+++ spec_quotes.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  9 19:17:34 2021
+
+@author: Benes
+"""
+
+creator8ball = ["yes creator mike",
+                "yes, human me",
+                "no creator mike",
+                "no, pls dont fire me creator mike",
+                "yes, please dont fire me creator mike",
+                "beep boop i mean no",
+                "yes, human mike",
+                "no, human me"]
+
+#squid suad
+franky8ball = ["for you franky, yes",
+               "for you franky, absolutely",
+               "for you franky, sure", 
+               "for you franky, i will only say yes", 
+               "for you franky, i agree that xerath sucks. also yes", 
+               "franky that sounds cap",
+               "yes your honor- i mean franky",
+               "yes, and also franky, xerath sucks",
+               "for you franky, i'll go with a yes",
+               "hell yeah franky", 
+               "unfortunately no, franky", 
+               "yikes",
+               "lmao",
+               "heck yeah franky",
+               "no your honor- i mean franky",
+               "for you franky, yis",
+               "just because it's you franky, no",
+               "yes, and also you should play smite zyra mid", 
+               "no franky im sorry", 
+               "NOPE sorry franky"]
+
+jacky8ball = ["yes but also you are a furry jacky",
+              "yes jacky",
+              "no jacky",
+              "no, furry",
+              "yes, furry",
+              "absolutely jacky"]
+
+megan8ball = ["haha, shut up megan",
+              "haha, shut up megan",
+              "yes megan",
+              "absolutely megan",
+              "indeed megan",
+              "heck yeah megan",
+              "yes megan",
+              "no megan why", 
+              "just because it's you megan, yes"]
+
+britney8ball = ["heck yeah britney",
+                "heck no britney",
+                "yes but only if you let mike pet miso",
+                "no britney but i will change my answer to yes if you let mike pet miso",
+                "heck no britney D:",
+                "yes, chogath main",
+                "yes, neeko main",
+                "no, neeko main"]
+
+nathan8ball = ["yes nathan",
+               "no nathan",
+               "yes, man from the void",
+               "no, man from the void"]
+
+kev8ball = ["yes, wizard rat uwu",
+            "no, wizard rat uwu",
+            "yes, vladimir main",
+            "no, vladimir main",
+            "no, reported",
+            "yes, but still reported",
+            "yes kev",
+            "no kev",
+            "no kev and also im a macro player"]
+
+ariel8ball = ["yes fgdu",
+              "yes ail",
+              "no ail",
+              "no ariel why",
+              "yes ariel :0",
+              "no ariel >:C",
+              "ariel jugilae??"]
+
+kiett8ball = ["yes kiett, also teach me swain pls",
+              "yes kiett",
+              "no kiett"]
+
+#franky server
+kelly8ball = ["no kelly",
+              "yes junting",
+              "no junting",
+              "yes kelly pls",
+              "i mean okay kelly yeah",
+              "yes xira17",
+              "no xira17",
+              "okay kelly yes"]
+
+lexie8ball = ["yeah okay scarletheartUWU",
+              "okay lexie", 
+              "no lexie",
+              "yes lexie",
+              "no u lexie"]
+
+kyle8ball = ["yes, kyle who lived with me at 901 nobel drive",
+             "no, kyle who lived with me at 901 nobel drive"]
+
+alvi8ball = ["lmao sure alvi",
+             "okay smelly",
+             "yes alvi",
+             "no alvi"]
+
+hannah8ball = ["yes, hannah", 
+               "nope hannah",
+               "no hannah",
+               "lmao no hannah",
+               "yes hannah but also why",
+               "yes hannah streamer extraordinaire"]
+
+#HGS
+mikey8ball = ["yes, enlightened one",
+              "no, enlightened one",
+              "yes, enlightened mikey",
+              "no, enlightened mikey",
+              "yes, book writer man",
+              "no, book writer man",
+              "just because it's you mikey, no. BITCH",
+              "just because it's you mikey, yes"]
+
+brandon8ball = ["yes brandon",
+                "no brandon",
+                "fuck no brandon",
+                "fuck yes brandon"]
+
+mathieu8ball = ["yes mathieu",
+                "no mathieu",
+                "hell no mathieu",
+                "yes mathieu, also order more magic spoon",
+                "yes, genius of color",
+                "im sorry, no, genius of color",
+                "yes, movie-maker",
+                "no, movie-maker",
+                "yes, paragon of netflix",
+                "no, paragon of netflix"]
